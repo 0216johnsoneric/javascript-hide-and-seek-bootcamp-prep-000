5a55d67ea5ac1526a01585e43bca1aaa13a177cd
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-01 23:28:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function getFirstSelector(selector){
-  return document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector(".target")
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  var ranked = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < ranked.length; i++) {
+    ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n
+  }
 }
